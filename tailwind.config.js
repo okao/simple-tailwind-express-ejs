0d11007ac620c10b1a3eb09de329f397dde514da
Hamzath Anees <hamzath.anees@mib.com.mv>
2023-11-13 10:34:12 +0500

    Preline

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ module.exports = {
     //load any file in the views folder
     "./views/*.{ejs,html,js,ts,jsx,tsx,vue}",
     "./views/**/*.{ejs,html,js,ts,jsx,tsx,vue}",
+
+    //load preline styles
+    "./node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [
+    require("preline/plugin"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
